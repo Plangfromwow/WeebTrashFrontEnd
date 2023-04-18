b73a2b5426611bf8f941d7af95f161a0ebce4581
plangfromwow <jacobwantswow@gmail.com>
2023-04-17 21:03:48 -0400

    Added pictures, added icons, added footer, hero, anime figure, added lots of goodies and things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    FooterComponent,
+    AboutSectionComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HeroComponent },
+      { path: 'about', component: AboutSectionComponent },
+      { path: 'contact', component: ContactUsComponent }
+    ]),
+    FontAwesomeModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
